a2e0d4e3ff6579339ad758d42758d65c68489d97
dnalverson <88463574+dnalverson@users.noreply.github.com>
2023-02-03 15:09:01 -0500

    adding install requirements

--- ThinFilmDataCreation/ThinFilmDataCreation/__init__.py
+++ ThinFilmDataCreation/ThinFilmDataCreation/__init__.py
@@ -1,9 +1,9 @@
 # init file for the wrpoly package
 # Authors: Danielle Alverson
 # created: 2023-02-2
-# last modified: 2023-02-2
-# version: 0.0.0
+# last modified: 2023-03-2
+# version: 0.0.8
 
 # import modules
 
-from .ThinFilDataCreation import *
+from .ThinFilmDataCreation import *
--- ThinFilmDataCreation/setup.py
+++ ThinFilmDataCreation/setup.py
@@ -1,14 +1,14 @@
 # setup.py file for the TFData package
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='ThinFilmDataCreation',
-    version='0.0.5',
+    version='0.0.9',
     description='To create simulated 2D images of calibrant materials on single crystal substrates and integrate them with multiple integration patterns',
     long_description= 'This package is designed to create simulated 2D images of calibrant materials on different orientations of single crystal substrates and integrate them to get multiple integration patterns. The integration patterns can be used for further analysis to extract and separate the single crystal background and calibrant material of interest.',
     author='Danielle N. Alverson',
     author_email='dalverson@ufl.edu',
-    packages=find_packages(),
+    packages = ['ThinFilmDataCreation',],
     url='https://github.com/dnalverson/thinfilm-toy-data-tool/'
-    #install_requirements = ['numpy', 'matplotlib', 'scipy', 'pyFAI', 'pyFAI.gui', 'pyFAI.detectors', 'pyFAI.azimuthalIntegrator']
+    install_requires = ['matplotlib', 'pyFAI', 'scipy', 'numpy', ]
 )
