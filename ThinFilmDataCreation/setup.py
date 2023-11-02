6f473b4f8aeed054e54ff0b4684c7f36b3c6bf8a
dnalverson <88463574+dnalverson@users.noreply.github.com>
2023-11-02 09:17:39 -0400

    updating name

--- ThinFilmDataCreation/setup.py
+++ ThinFilmDataCreation/setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name='ThinFilmDataCreation',
+    name='SimDat2D',
     version='0.1.0',
     description='To create simulated 2D images of calibrant materials on single crystal substrates and integrate them with multiple integration patterns',
     long_description= 'This package is designed to create simulated 2D images of calibrant materials on different orientations of single crystal substrates and integrate them to get multiple integration patterns. The integration patterns can be used for further analysis to extract and separate the single crystal background and calibrant material of interest.',
     author='Danielle N. Alverson',
     author_email='dalverson@ufl.edu',
-    packages = ['ThinFilmDataCreation',],
+    packages = ['SimDat2D',],
     url='https://github.com/dnalverson/thinfilm-toy-data-tool/',
     install_requires = ['matplotlib', 'pyFAI', 'scipy', 'numpy']
 )
