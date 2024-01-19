b0e2a9c15185542b0c86f6035633f52058516b24
dnalverson <dalverson@ufl.edu>
2024-01-19 09:39:23 -0500

    fixing setup.py

--- SimDAT2D/build/bdist.win-amd64/wheel/SimDat2D/__init__.py
+++
@@ -1,9 +0,0 @@
-# init file for the DAT2D package
-# Authors: Danielle Alverson
-# created: 2023-02-2
-# last modified: 2023-11-2
-# version: 0.1.0
-
-# import modules
-
-from .SimDAT2D import *
--- SimDAT2D/setup.py
+++ SimDAT2D/setup.py
@@ -1,14 +1,15 @@
+#https://github.com/dnalverson/SimDAT2D/
 # setup.py file for the TFData package
 from setuptools import setup
 
 setup(
-    name='SimDat2D',
+    name='SimDAT2D',
     version='0.1.0',
     description='To create simulated 2D images of calibrant materials on single crystal substrates and integrate them with multiple integration patterns',
     long_description= 'This package is designed to create simulated 2D images of calibrant materials on different orientations of single crystal substrates and integrate them to get multiple integration patterns. The integration patterns can be used for further analysis to extract and separate the single crystal background and calibrant material of interest.',
     author='Danielle N. Alverson',
     author_email='dalverson@ufl.edu',
-    packages = ['SimDat2D',],
-    url='https://github.com/dnalverson/thinfilm-toy-data-tool/',
+    packages = ['SimDAT2D'],
+    url='https://github.com/dnalverson/SimDAT2D/',
     install_requires = ['matplotlib', 'pyFAI', 'scipy', 'numpy']
 )
